918636c9c7f05db1bf3d1c91cfc48aa34006b781
Mariana Guidoli <maruguidoli@gmail.com>
2024-12-22 02:28:42 +0000

    Segundo uso parcial de AppShell(Mantine) para el NavBar

--- app/root.tsx
+++ app/root.tsx
@@ -21,7 +21,10 @@ import {
     MantineProvider, 
     Group, 
     TextInput, 
-    Button, 
+    Button,
+    ScrollArea,
+    Skeleton,
+    Burger, 
      } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 
@@ -76,60 +79,67 @@ export default function App({ children }: { children: React.ReactNode}) {
       <body>
         <MantineProvider>
           <AppShell navbar={{ width: 300, breakpoint: 'sm', collapsed: { mobile: !opened }}} padding="md">
+            <AppShell.Header>
+              <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+            </AppShell.Header>
             <AppShell.Navbar p="md">
-              <Group>
-                <Form
-                  id="search-form"
-                  onChange={(event) => {
-                    const isFirstSearch = q === null;
-                    submit(event.currentTarget, {
-                      replace: !isFirstSearch,
-                    });
-                  }}
-                  role="search"
-                >
-                  <TextInput
-                    className={searching ? "loading" : ""}
-                    defaultValue={q || ""}
-                    id="q"
-                    name="q"
-                    placeholder="Search"
-                    type="search" 
-                  />
-                </Form>
-                <Form>
-                  <Button variant="outline" type="submit">New</Button>
-                </Form>
-              </Group>
-              <nav>
+              <AppShell.Section>
+                <Group>
+                  <Form
+                    id="search-form"
+                    onChange={(event) => {
+                      const isFirstSearch = q === null;
+                      submit(event.currentTarget, {
+                        replace: !isFirstSearch,
+                      });
+                    }}
+                    role="search"
+                  >
+                    <TextInput
+                      className={searching ? "loading" : ""}
+                      defaultValue={q || ""}
+                      id="q"
+                      name="q"
+                      placeholder="Search"
+                      type="search" 
+                    />
+                  </Form>
+                  <Form>
+                    <Button variant="outline" type="submit">New</Button>
+                  </Form>
+                </Group>
+              </AppShell.Section>
+              <AppShell.Section grow my="md" component={ScrollArea}>
                 {contacts.length ? (
                   <ul>
                     {contacts.map((contact) => (
-                      <li key={contact.id}>
-                        <NavLink
-                          className={({ isActive, isPending }) =>
-                          isActive
-                            ? "active"
-                            : isPending
-                            ? "pending"
-                            : ""
-                          }
-                          to={`contacts/${contact.id}`} 
-                        >
-                          <Link to={`contacts/${contact.id}`}>
-                            {contact.first || contact.last ? (
-                              <>
-                                {contact.first} {contact.last}
-                              </>
-                            ) : (
-                              <i>No Name</i>
-                            )}{" "}
-                            {contact.favorite ? (
-                              <span>★</span>
-                            ) : null}
-                          </Link>
-                        </NavLink>
-                      </li>
+                      <Skeleton key={contact.id} h={28} mt="md" animate={false} visible={false}>
+                        <Button fullWidth>
+                          <NavLink
+                            className={({ isActive, isPending }) =>
+                            isActive
+                              ? "active"
+                              : isPending
+                              ? "pending"
+                              : ""
+                            }
+                            to={`contacts/${contact.id}`} 
+                          >
+                            <Link to={`contacts/${contact.id}`}>
+                              {contact.first || contact.last ? (
+                                <>
+                                  {contact.first} {contact.last}
+                                </>
+                              ) : (
+                                <i>No Name</i>
+                              )}{" "}
+                              {contact.favorite ? (
+                                <span>★</span>
+                              ) : null}
+                            </Link>
+                          </NavLink>
+                        </Button>
+                      </Skeleton>
                     ))}
                   </ul>
                 ) : (
@@ -137,7 +147,7 @@ export default function App({ children }: { children: React.ReactNode}) {
                     <i>No contacts</i>
                   </p>
                 )}
-              </nav>
+              </AppShell.Section>
             </AppShell.Navbar>
             <AppShell.Main>
               Main
